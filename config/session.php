5435b8b7ca806e3a58e628c6cbd1164c6cdba336
yoliMunozS <yolanda.ms1973@gmail.com>
2021-12-19 18:52:11 +0100

    cierra sesión al cerrar navegador

--- config/session.php
+++ config/session.php
@@ -33,7 +33,7 @@ return [
 
     'lifetime' => env('SESSION_LIFETIME', 120),
 
-    'expire_on_close' => false,
+    'expire_on_close' => true,  //cierra sesión al salir del navegador
 
     /*
     |--------------------------------------------------------------------------
