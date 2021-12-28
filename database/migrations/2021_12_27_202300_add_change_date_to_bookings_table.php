cace00cb6941f091e3c6af2ae5149b7a413c419a
yoliMunozS <yolanda.ms1973@gmail.com>
2021-12-28 18:27:22 +0100

    cambio timestamp por date

---
+++ database/migrations/2021_12_27_202300_add_change_date_to_bookings_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddChangeDateToBookingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('bookings', function (Blueprint $table) {
+            //
+            $table->date('start')->nullable()->change();
+            $table->date('end')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('bookings', function (Blueprint $table) {
+            //
+            $table->dropColumn('start');
+            $table->dropColumn('end');
+        });
+    }
+}
